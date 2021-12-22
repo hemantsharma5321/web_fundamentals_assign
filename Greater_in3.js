8a3011e4ceb854c47f2e1280eae672eb91070b3b
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2021-12-22 15:28:34 +0530

    Update Greater_in3.js

--- Greater_in3.js
+++ Greater_in3.js
@@ -1,6 +1,12 @@
-Var num1=10;
+var num1=10;
 var num2=20;
 var num3=30;
 if(num1>num2 && num1>num3){
 console.log(num1 + "is greater");
+}
+else if(num2>num1 && num2>num3){
+    console.log(num2 + "is greater");
+}
+else{
+    console.log(num3 + "is greater");
 }
