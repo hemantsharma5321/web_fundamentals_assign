c43dca263e3d22b1ea54a02487c1aa249653a361
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2021-12-31 09:37:13 +0530

    Create Dec_20_1.js

---
+++ Dec_20_1.js
@@ -0,0 +1,10 @@
+//Write a program to check whether the number is greater than 10 or not
+var num=prompt("enter number");
+
+if(num>10){
+    console.log("greater than 10");
+
+}
+else{
+    console.log("less than 10");
+}
